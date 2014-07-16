e97e122d1b44158f8a730ed3c3c46302a92e9142
Tim Green <iamtimgreen@gmail.com>
2014-07-16 21:55:53 +1000

    Fix regexp.

--- unused_imports_scala.sh
+++ unused_imports_scala.sh
@@ -8,9 +8,9 @@ remove_import() {
 
 check_import() {
   local import="$1"
-  local c=$(echo $import | sed "s/^.*\.\([a-zA-Z]\+\)\s*$/\\1/")
+  local c=$(echo $import | sed "s/^.*\.\([a-zA-Z0-9]\+\)\s*$/\\1/")
   local f="$2"
-  grep -v "^\s*import [a-zA-Z.]\+\s*$" "$f" | grep "\b$c\b" &> /dev/null || {
+  grep -vF "$import" "$f" | grep "\b$c\b" &> /dev/null || {
     echo " - $import"
     remove_import "$import" "$f"
   }
@@ -20,7 +20,7 @@ unused_imports() {
   local f="$1"
   echo "$f"
   # TODO(timgreen): support import java.util.{ List => JList, Date }
-  imports=$(grep "^\s*import [a-zA-Z.]\+\s*$" "$f" | grep -v "scala.language.")
+  imports=$(grep "^\s*import [a-zA-Z0-9.]\+\s*$" "$f" | grep -v "scala.language.")
   for import in ${imports[@]}; do
     check_import "$import" "$f"
   done
