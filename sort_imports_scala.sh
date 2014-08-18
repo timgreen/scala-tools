b612f2bd5e47725cb32a7cd52f3838bbb97f011d
Tim Green <iamtimgreen@gmail.com>
2014-08-18 15:39:04 +1000

    Use awk to get imports block range.

--- sort_imports_scala.sh
+++ sort_imports_scala.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-print_imports_line_number() {
-  awk '/^import / {print NR}' $1
-}
-
-turn_into_block_range() {
-  local b=-1
-  local e=-1
-  while read ln; do
-    if (( $b == -1 )); then
-      b=$ln
-      e=$ln
-    elif (( $ln == $e + 1 )); then
-      e=$ln
-    else
-      if (( $b < $e )); then
-        echo $b:$e
-      fi
-      b=$ln
-      e=$ln
-    fi
-  done
-  if (( $b != -1 )) && (( $b < $e )); then
-    echo $b:$e
-  fi
+print_imports_range() {
+  awk '
+    BEGIN {
+      b = -1
+      e = -1
+    }
+    /^import / {
+      if ( b == -1 ) {
+        b = NR
+        e = NR
+      } else if ( NR == e + 1 ) {
+        e = NR
+      } else {
+        if ( b < e ) {
+          print b ":" e
+        }
+        b = NR
+        e = NR
+      }
+    }
+    END {
+      if ( b < e ) {
+        print b ":" e
+      }
+    }
+  ' $1
 }
 
 sort_import_block() {
@@ -43,8 +45,7 @@ sort_import_block() {
 sort_imports() {
   f=$1
   echo "processing $f" 1>&2
-  print_imports_line_number $f \
-    | turn_into_block_range \
+  print_imports_range $f \
     | sort_import_block $f
 }
 
